e9d3c55f5957a9742564cec6055c9abc29934be5
Vageesh <65950037+iamvageesh@users.noreply.github.com>
2023-08-03 23:24:34 +0530

    added get feature to script install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
 echo "Installing the setup"
+wget -O configuration.nix https://raw.githubusercontent.com/iamvageesh/nix_config/main/configuration.nix
+sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.backup
+sudo cp configuration.nix /etc/nixos/configuration.nix
