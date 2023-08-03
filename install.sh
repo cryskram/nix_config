338c482adb54641aa321e7590846069c89b8a5a8
Vageesh <65950037+iamvageesh@users.noreply.github.com>
2023-08-03 23:35:30 +0530

    :rocket: condition for building the system install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 echo "Installing the setup"
+
 wget -O configuration.nix https://raw.githubusercontent.com/iamvageesh/nix_config/main/configuration.nix
+
 sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.backup
 sudo cp configuration.nix /etc/nixos/configuration.nix
+
+read -p "Do you want to rebuild the system with new configuration[Y/n]: " res
+
+if [ $res == "y" || $res == "Y" ]
+then
+  sudo nixos-rebuild switch
+else
+  echo "You can run the build command manually later"
+fi
